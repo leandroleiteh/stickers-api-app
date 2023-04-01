7cc1ce70f77f2901ab7ce921b1e93ce729fd0a2d
leandroleiteh <leandroluz2016162gmail.com>
2023-03-31 22:16:56 -0300

    Primeiro commit, onde contem a aplicação central

---
+++ sticker-wpp/src/ChaveApi.java
@@ -0,0 +1,15 @@
+public class ChaveApi {
+
+    private String chave = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+
+    
+
+    public String getChave() {
+        return chave;
+    }
+
+    public void setChave(String chave) {
+        this.chave = chave;
+    }
+
+}
